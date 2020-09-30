250904a286261ef841c91ea68d13e5efbcfde165
gurpreets0804 <72162250+gurpreets0804@users.noreply.github.com>
2020-10-01 00:13:25 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,14 @@
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    fac =1
+    for i in range(1,n+1):
+        fac=fac*i
+    print(fac)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    extraLongFactorials(n)
